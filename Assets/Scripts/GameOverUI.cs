4daececa13e99e319a9932d179f8ec3ca48a8811
julianschmitt123 <julianschmitt76@gmail.com>
2020-02-17 16:16:57 +0100

    Push

---
+++ Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour
+{
+    
+    public void Quit()
+    {
+        Debug.Log("Quit Game");
+
+
+        Application.Quit();
+    }
+
+
+    public class RestartGame : MonoBehaviour
+    {
+        public void Restart()
+        {
+            Debug.Log("Restart");
+            SceneManager.LoadScene(SampleScene);
+        }
+
+
+    }
+}
